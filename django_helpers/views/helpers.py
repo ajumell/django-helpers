9ee0174d201bc82302fc88dbc81d930c183f5fb5
Muhammed K K <ajumell@xeoscript.com>
2012-05-26 06:50:05 +0530

    Added render json helper. Added option for rendering ajax datas in form.

--- django_helpers/views/helpers.py
+++ django_helpers/views/helpers.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader, Context
+from django.utils import simplejson
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -34,11 +35,18 @@ def render_template(request, template, data=None, url_params=None, **kwargs):
     if url_params is not None:
         if data is None:
             data = dict()
+
         for url_param in url_params:
             param_value = kwargs.get(url_param, '')
-            if param_value != "0":
+            if param_value != "":
                 data[url_param] = param_value
     return render_to_response(template, request, data)
+
+
+def render_json(dictionary):
+    string = simplejson.dumps(dictionary)
+    mimetype = 'application/javascript'
+    return HttpResponse(string, mimetype=mimetype)
 
 
 def render_to_string(template, values=None):
@@ -101,11 +109,11 @@ def form_view(request,
               submit_button="Submit",
               error_message="Please correct the errors.",
               template="generic/form.html",
+              ajax_template="partials/form.html",
               obj_instance=None,
               custom_form_arguments=None,
               template_dict=None,
               obj_url_params=None,
-              *args,
               **kwargs
 ):
     """
@@ -125,6 +133,9 @@ def form_view(request,
     if obj_instance is not None:
         custom_form_arguments['instance'] = obj_instance
 
+    if template_dict is None:
+        template_dict = {}
+
     if request.method == "POST":
         if is_multipart(Form):
             form = Form(request.POST, request.FILES, **custom_form_arguments)
@@ -140,28 +151,41 @@ def form_view(request,
                     setattr(obj, param, kwargs.get(param))
 
             obj.save()
-
-            if success_message is not None:
-                messages.success(request, success_message)
-            if success_view_args is not None:
-                args = list()
-                for argument in success_view_args:
-                    args.append(kwargs.get(argument))
-                return redirect(success_view, tuple(args))
+            if request.is_ajax():
+                response_dict = {
+                    "success": True,
+                    "message": success_message
+                }
+                return render_json(response_dict)
             else:
-                return redirect(success_view)
+                if success_message is not None:
+                    messages.success(request, success_message)
+
+                if success_view_args is not None:
+                    args = list()
+                    for argument in success_view_args:
+                        args.append(kwargs.get(argument))
+                    return redirect(success_view, tuple(args))
+                else:
+                    return redirect(success_view)
         else:
-            form.error_message = error_message
+            if request.is_ajax():
+                rdict = {'error': True}
+                d = {}
+                for e in form.errors:
+                    d.update({e: form.errors[e]})
+                rdict.update({'errors': d})
+                return render_json(rdict)
+            template_dict['form_error_message'] = error_message
     else:
         form = Form(**custom_form_arguments)
 
-    form.heading = form_heading
-    form.submit = submit_button
-
-    if template_dict is None:
-        template_dict = {}
-
     template_dict['form'] = form
+    template_dict['form_submit'] = submit_button
+    template_dict['form_heading'] = form_heading
+
+    if request.is_ajax():
+        template = ajax_template
 
     return render_to_response(template, request, template_dict)
 
